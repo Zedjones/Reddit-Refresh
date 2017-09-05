4e6cd841194cea0d35e2bcae1b5700f431576fe6
Nicholas Jones <dojoman19@gmail.com>
2017-09-05 13:12:26 -0400

    Able to access list of Pushbullet devices given an access token

--- RedditRefresh.py
+++ RedditRefresh.py
@@ -5,8 +5,19 @@ def main():
     token = input("Enter your Pushbullet access token " + \
             "(found on the Account Settings page): ")
     header = 'Access-Token: ' + token
-    res = requests.get('https://api.pushbullet.com/v2/users/me', auth=(token, ''))
-    print(res.text)
+    accinfo = requests.get('https://api.pushbullet.com/v2/users/me', auth=(token, ''))
+    out = json.loads(accinfo.text)
+    deviceinfo = requests.get('https://api.pushbullet.com/v2/devices', auth=(token, ''))
+    device_dict = get_devices(json.loads(deviceinfo.text))
+
+
+def get_devices(deviceout):
+    device_dict = {}
+    for i in deviceout["devices"]:
+       if "nickname" in i and "iden" in i:
+           device_dict[i["iden"]] = i["nickname"]
+    return device_dict
+           
 
 
 main()
