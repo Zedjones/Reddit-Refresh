f6f844cc76808aa7b248159cad349449d4b2912d
Nicholas Jones <dojoman19@gmail.com>
2017-10-20 17:47:41 -0400

    Continued working on HTML parsing, made some small changes

--- subreddit_parser.py
+++ subreddit_parser.py
@@ -6,6 +6,6 @@ response = urllib3.connection_from_url('https://www.reddit.com/r/mechmarket/\
         search?q=Planck&sort=new&restrict_sr=on&t=all')
 r = response.urlopen('GET', url)
 soup = BeautifulSoup(r.data.decode("utf-8"))
-mydivs = soup.findAll("div", {"class": "search-result-group"})
+mydivs = soup.findAll("div", {"class": "contents"})
 for div in mydivs:
-    print(div)
+    case = BeautifulSoup(str(div.contents))
