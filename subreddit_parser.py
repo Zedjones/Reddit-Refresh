c0ac1e3b2e325b8d0450611acc56d6aa5021f5f5
Nicholas <dojoman19@gmail.com>
2017-10-21 19:08:31 -0400

    Continued working on the subreddit parsing

--- subreddit_parser.py
+++ subreddit_parser.py
@@ -1,14 +1,20 @@
 from bs4 import BeautifulSoup
 import urllib3
 
-url = "https://www.reddit.com/r/mechmarket/search?q=Planck&sort=new&restrict_sr=on&t=all"
-response = urllib3.connection_from_url('https://www.reddit.com/r/mechmarket/\
-        search?q=Planck&sort=new&restrict_sr=on&t=all')
-r = response.urlopen('GET', url)
-soup = BeautifulSoup(r.data.decode("utf-8"))
-contents = soup.findAll("div", {"class": "contents"})
-soup = BeautifulSoup(str(contents))
-headers = soup.findAll("header", {"class": "search-result-header"})
-for header in headers:
+def get_results():
+    resultdict = {}
+    urllib3.disable_warnings()
+    url = "https://www.reddit.com/r/mechmarket/search?q=Planck&sort=new&restrict_sr=on&t=all"
+    response = urllib3.connection_from_url(url)
+    r = response.urlopen('GET', url)
+    soup = BeautifulSoup(r.data.decode("utf-8"), "html.parser")
+    contents = soup.find("div", {"class": "contents"})
+    for header in contents.children:
+        soup = BeautifulSoup(str(header), "html.parser")
+        link = soup.find('a', href=True)
+        resultdict[link['href']] = link.text
+    print(resultdict)
+    return resultdict
 
+get_results()
 
