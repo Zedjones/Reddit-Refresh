0fb51a7ca9fb00276719a9e78b98dcf1a2669a2e
Nicholas Jones <dojoman19@gmail.com>
2017-10-25 13:45:44 -0400

    Added some in-function comments

--- subreddit_parser.py
+++ subreddit_parser.py
@@ -13,26 +13,38 @@ titles
 '''
 def get_results(sb, search, sort="new", flair=False):
     resultdict = {}
+    #TODO make the connection secure so I don't have to do this
     urllib3.disable_warnings()
-    sorts = ["new", "top", "relevance"]
+    sorts = ["new", "top", "relevance"] #three sorting options for Reddit search
+    #properly format this for url construction
     if "/r/" not in sb:
         sb = "/r/" + sb
+    #default to new sorting if invalid sort is provided
     if sort.lower() not in sorts:
         sort = "new"
+    #construct the URL
     url = "https://www.reddit.com%s/\
 search?q=%s&sort=%s&restrict_sr=on&t=all" % (sb, search, sort.lower())
+    #get HTML to parse and initialize parser
     response = urllib3.connection_from_url(url)
     r = response.urlopen('GET', url)
     soup = BeautifulSoup(r.data.decode("utf-8"), "html.parser")
+    #find contents class
     contents = soup.find("div", {"class": "contents"})
+    #for each entry in the search
     for header in contents.children:
+        #specific parsing to make sure that this works for subreddits
+        #with custom CSS
         soup = BeautifulSoup(str(header), "html.parser")
         srheader = soup.find("header")
         soup =  BeautifulSoup(str(srheader), "html.parser")
+        #get the flair if the user requested it
         if flair:
             flairt = soup.find("span", {"class": "linkflairlabel"})
             flairt = flairt.text
+        #get the part of the entry that contains the link and title
         link = soup.find('a', href=True)
+        #prepend the flair to the title if requested or don't otherwise
         if flair:
             resultdict[link['href']] = flairt + " " + link.text
         else:
@@ -40,4 +52,5 @@ search?q=%s&sort=%s&restrict_sr=on&t=all" % (sb, search, sort.lower())
     print(resultdict)
     return resultdict
 
+#test run
 get_results("mechmarket", "Planck", "new")
