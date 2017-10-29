35870935fb5c391079e4ef467e917f846ab03ff6
Nicholas <dojoman19@gmail.com>
2017-10-29 00:07:35 -0400

    Added documentation for all changes

--- reddit_refresh.py
+++ reddit_refresh.py
@@ -21,23 +21,26 @@ def main():
     #otherwise,  open a new config file for writing
     else:
         configf = open(str(Path.home())+"/.config/reddit-refresh/config", 'w')
-    #if it's the firstrun or the config file is malformed
+    #create new config parser
     config = configparser.ConfigParser()
+    #read the config file
     config.read(str(Path.home())+"/.config/reddit-refresh/config")
+    #if it's the first run or there is no User Info section 
     if(firstrun or "User Info" not in config):
-        #get access token from user and write it to the first line
+        #get access token from user
         token = input("Enter your Pushbullet access token " + \
                 "(found on the Account Settings page): ")
+        #set entry in User Info for token to the token provided
         config['User Info'] = {}
         config['User Info']['token'] = token
-    #otherwise, get the token and do some formatting
+    #otherwise, get the token from the config file
     else:
         token = config['User Info']['token']
     #get the account info from the Pushbullet API
     accinfo = requests.get('https://api.pushbullet.com/v2/users/me', auth=(token, ''))
     #get the text from the json object
     out = json.loads(accinfo.text)
-    #if line with token is the only line in the file
+    #if there is no Devices section in the config file
     if('Devices' not in config):
         #get device info and creates a device dictionary with this info
         deviceinfo = requests.get('https://api.pushbullet.com/v2/devices', auth=(token, ''))
@@ -46,35 +49,44 @@ def main():
         choice_list = create_choice_list(device_dict)
         #get the devices to push using the choice list
         devices_to_push = get_devices_to_push(choice_list)
-        #write each device to the config file, using a standard format
+        #create an entry for each device in the Devices section
         config['Devices'] = {}
         for i in range(len(devices_to_push)):
             print(devices_to_push[i][0])
             config['Devices'][devices_to_push[i][1]] = \
                     devices_to_push[i][0]
-    #if line with token is not the only line in file 
+    #if there is a Devices section
     else:
         #create empty dictionary
         devices_to_push = {}
-        #go to second line of the file 
-        #read in each of the devices
+        #read each entry in Devices and put it in the dictionary
         for entry in config['Devices']:
             devices_to_push[config['Devices'][entry]] = entry
+    #temporary search results test 
+    #TODO get query and subreddit, etc. from the user
     search_results = get_results("mechanicalkeyboards", "Canvas")
+    #create list to hold the previous results
     previous_results = []
-    send_a_push_test(devices_to_push, token)
+    #if visited_sites is a file, open it for reading and writing
     if os.path.isfile(str(Path.home())+"/.config/reddit-refresh/visited_sites.txt"):
         seen = open(str(Path.home())+"/.config/reddit-refresh/visited_sites.txt", 'r+')
+        #for each url in the file
         for line in seen:
+            #add it to the list
             previous_results.append(line.strip())
+    #if it is not a file, create the file and open it for writing
     else:
         seen = open(str(Path.home())+"/.config/reddit-refresh/visited_sites.txt", 'w')
+        #write each url to the file, close it, and reopen it for r+w
         for key in search_results:
             seen.write(key + "\n")
         seen.close()
         seen = open(str(Path.home())+"/.config/reddit-refresh/visited_sites.txt", 'r+')
     noMatches = True
+    #if there were any previous results
     if(len(previous_results) > 0):
+        #only send a push if a result hasn't been seen before, and then 
+        #write the url to the file
         for key in search_results:
             if key not in previous_results:
                 send_a_push_link(devices_to_push, token, \
@@ -82,17 +94,20 @@ def main():
                 seen.write(key + "\n")
             else:
                 noMatches = False
+        #if we haven't seen any of these urls before, we can simply erase
+        #the  file and start over
         if noMatches:
             seen.seek(0)
             seen.truncate()
             for key in search_results:
                 seen.write(key + "\n")
+    #if there were no previous results, just send the first result
     else:
         for key in search_results:
             send_a_push_link(devices_to_push, token, \
                     key, search_results[key])
             break
-    #close file as standard practice
+    #close file as standard practice, and write config to file
     config.write(configf)
     seen.close()
     configf.close()
@@ -138,8 +153,6 @@ def get_devices_to_push(choice_list):
 
 def send_a_push_test(devices_to_push, token):
     for device in devices_to_push:
-        print(device)
-        print(token)
         url = "https://api.pushbullet.com/v2/pushes"
         data = {"body": "This is a test.", "title": "Test", "type": \
                 "note", "device_iden": device}
@@ -147,7 +160,6 @@ def send_a_push_test(devices_to_push, token):
         data_json = json.dumps(data)
         payload = {"json_payload": data_json}
         result = requests.post(url, data=data_json, headers=headers)
-        print(result)
 
 def send_a_push_link(devices_to_push, token, link, title):
     for device in devices_to_push:
