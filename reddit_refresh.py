141c4a7055ee8b98a1a01252fd5746778b3efdfb
Nicholas <dojoman19@gmail.com>
2017-10-28 23:30:32 -0400

    Switched config file to use configparser instead of directly writing to a file

--- reddit_refresh.py
+++ reddit_refresh.py
@@ -5,46 +5,40 @@ import requests
 import os
 from pathlib import Path
 from subreddit_parser import get_results
+import configparser
 
 def main():
     #assume that this is the first run
     firstrun = True
+
     #make the directory to hold the config if it doesn't exist
     if not os.path.exists(str(Path.home())+"/.config/reddit-refresh"):
         os.makedirs(str(Path.home())+"/.config/reddit-refresh")
     #open the config file for reading and writing if it exists, set firstRun to false
     if os.path.isfile(str(Path.home())+"/.config/reddit-refresh/config"):
-        config = open(str(Path.home())+"/.config/reddit-refresh/config", 'r+')
+        configf = open(str(Path.home())+"/.config/reddit-refresh/config", 'r+')
         firstrun = False
     #otherwise,  open a new config file for writing
     else:
-        config = open(str(Path.home())+"/.config/reddit-refresh/config", 'w')
+        configf = open(str(Path.home())+"/.config/reddit-refresh/config", 'w')
     #if it's the firstrun or the config file is malformed
-    if(firstrun or "token" not in config.readline()):
-        secondline = config.tell()
+    config = configparser.ConfigParser()
+    config.read(str(Path.home())+"/.config/reddit-refresh/config")
+    if(firstrun or "User Info" not in config):
         #get access token from user and write it to the first line
         token = input("Enter your Pushbullet access token " + \
                 "(found on the Account Settings page): ")
-        config.write("token=" + token + "\n")
-        #close and repon the file for reading and writing
-        config.close()
-        config = open(str(Path.home())+"/.config/reddit-refresh/config", 'r+')
+        config['User Info'] = {}
+        config['User Info']['token'] = token
     #otherwise, get the token and do some formatting
     else:
-        #get location of second line of the file for later use
-        secondline = config.tell()
-        config.seek(0)
-        token = config.readline().split('=')[1].strip()
-        tmp = config.readline()
-        if(tmp == "" or tmp == "\n"):
-            config.write("\n")
-        config.seek(secondline)
+        token = config['User Info']['token']
     #get the account info from the Pushbullet API
     accinfo = requests.get('https://api.pushbullet.com/v2/users/me', auth=(token, ''))
     #get the text from the json object
     out = json.loads(accinfo.text)
     #if line with token is the only line in the file
-    if('token' in config.readline().strip()):
+    if('Devices' not in config):
         #get device info and creates a device dictionary with this info
         deviceinfo = requests.get('https://api.pushbullet.com/v2/devices', auth=(token, ''))
         device_dict = get_devices(json.loads(deviceinfo.text))
@@ -53,27 +47,22 @@ def main():
         #get the devices to push using the choice list
         devices_to_push = get_devices_to_push(choice_list)
         #write each device to the config file, using a standard format
+        config['Devices'] = {}
         for i in range(len(devices_to_push)):
-            if(i == len(devices_to_push)):
-                config.write(devices_to_push[i][0] + ",")
-                config.write(devices_to_push[i][1])
-            else:
-                config.write(devices_to_push[i][0] + ",")
-                config.write(devices_to_push[i][1] + "\n")
+            print(devices_to_push[i][0])
+            config['Devices'][devices_to_push[i][1]] = \
+                    devices_to_push[i][0]
     #if line with token is not the only line in file 
     else:
         #create empty dictionary
         devices_to_push = {}
         #go to second line of the file 
-        config.seek(secondline)
         #read in each of the devices
-        device = config.readline()
-        while(device != "" and device != "\n"):
-            device = device.split(",")
-            devices_to_push[device[0]] = device[1].strip()
-            device = config.readline()
-    search_results = get_results("me_irl", "me irl")
+        for entry in config['Devices']:
+            devices_to_push[config['Devices'][entry]] = entry
+    search_results = get_results("mechanicalkeyboards", "Canvas")
     previous_results = []
+    send_a_push_test(devices_to_push, token)
     if os.path.isfile(str(Path.home())+"/.config/reddit-refresh/visited_sites.txt"):
         seen = open(str(Path.home())+"/.config/reddit-refresh/visited_sites.txt", 'r+')
         for line in seen:
@@ -104,8 +93,9 @@ def main():
                     key, search_results[key])
             break
     #close file as standard practice
-    config.close()
+    config.write(configf)
     seen.close()
+    configf.close()
 
 '''
 Create a dictionary of devices where the nickname is mapped to the id
@@ -149,13 +139,15 @@ def get_devices_to_push(choice_list):
 def send_a_push_test(devices_to_push, token):
     for device in devices_to_push:
         print(device)
+        print(token)
         url = "https://api.pushbullet.com/v2/pushes"
         data = {"body": "This is a test.", "title": "Test", "type": \
                 "note", "device_iden": device}
         headers = {'Content-Type': 'application/json', 'Access-Token': token}
         data_json = json.dumps(data)
         payload = {"json_payload": data_json}
-        requests.post(url, data=data_json, headers=headers)
+        result = requests.post(url, data=data_json, headers=headers)
+        print(result)
 
 def send_a_push_link(devices_to_push, token, link, title):
     for device in devices_to_push:
